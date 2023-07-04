c6f0d96efa174ae17b1519324ab94ff34166a412
Lagger2 Pascal Lambert <PascalLambert.Lagger2@edu.fh-kaernten.ac.at>
2023-07-04 22:55:14 +0200

    added swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const app = express();
 const app_ip = process.env.APPLICATION_HOST;
 const app_port = process.env.APPLICATION_PORT;
 
@@ -9,16 +11,34 @@ const db = require('./models');
 
 const userRouter = require("./routes/user.routes.js");
 
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express REST API for the soccer app',
+      version: '1.0.0',
+      description: 'This is a REST API for the soccer app developed in the lecture Medizinische Smarte Sensoren'
+    },
+    servers: [
+      {
+        url: 'http://' + app_ip + ':' + app_port
+      },
+    ],
+};
+  
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js'],
+};
+  
+const swaggerSpec = swaggerJSDoc(options);
+
 app.use(express.json());
 
 app.use("/user", userRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 db.sequelize.sync().then(() =>{ logger.info("sync was ok"); }).catch((err)=>{ logger.error(err); });
 
 app.listen(app_port, app_ip, () => {
     logger.info('App is listening on ' + app_ip + ' at ' + app_port);
 });
-
-app.get('/', (req, res) => {
-    res.json({info: 'Node.js, Express, and Postgres API'});
-})
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,41 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller.js");
 
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     parameters:
+ *       - in: path   
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: integer 
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The user's name.
+ *                         example: Leanne Graham
+*/
 router.get("/", userController.findAll);
 router.get("/:id", userController.findOne);
 
